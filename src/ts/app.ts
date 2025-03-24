1e0022f6ce7d90c351e104a515dd6afcb1760c41
ifattakhova <IFattakhova@beeline.uz>
2025-03-25 01:18:52 +0500

    ci: test to ruin deploy

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -7,7 +7,7 @@ class App {
 
   constructor() {
     this.cart = this.setCart();
-    App.router = new Router(this.cart);
+    // App.router = new Router(this.cart);
   }
 
   private setCart() {
@@ -16,7 +16,7 @@ class App {
     if (oldCartJson) {
       const oldCart: Cart = JSON.parse(oldCartJson);
       oldCart.basket ? (newCart.basket = oldCart.basket) : null;
-      oldCart.activePromoCodes ? (newCart.activePromoCodes = oldCart.activePromoCodes) : null;
+      // oldCart.activePromoCodes ? (newCart.activePromoCodes = oldCart.activePromoCodes) : null;
     }
     return newCart;
   }
